15e500b6ff1a4e3510e3427440e9388a2b744331
avrin002 <160397698+avrin002@users.noreply.github.com>
2024-02-23 23:39:09 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+// task1//
+
+
+// 1) // let num = 12
+// // let num2 = 8
+// // console.log(num * num2)
+
+// 2)// let num = 10
+// // let num2 = 2
+// // let x = num / num2
+// // console.log(x);
+
+// 3) let num = 20
+// let num2 = 17
+// console.log(num + num2)
+
+// 4)
+// var name
+// var surname
+// var year
+
+// name = 'Fuad'
+// surname = 'Rasulov'
+// year = 1999
+// var info = name + ' ' + surname + ' ' + year
+// console.log(info);
+
+
+
+// let num = 17
+// let num2 = 12
+// let result = num / num12
+
+
+// // TASK 2
+// // // let a = 1, b = 1;
+// // // let c = ++a;
+// // // let d = b++;
+// // // console.log(c) = c de 2 cixacaq
+// // // console.log(d) = d de 2 cixacaq
+// console.log(b) = b de 1 ciaxacaq ;
+
+
+// TASK 3
+// // let a = 2;
+// // let x = 1 + (a *= 2)
+// // console.log(x) = x de 5 cavabi cixacaq
+
+
+
+// TASK 4
+// console.log('test1', test); // test1 stringdir gorsenecek test ise undefined 
+// {
+//     let test = "something"
+//     console.log('test2', test); // test2 something gorsenecek 
+// }
+// console.log('test3', test); // test 3 stringdir gorsenecek digeri ise undefined 
+
+
+// console.log('test1', test); // test1 stringdir gorsenecek test ise undefined 
+// {
+//     var test = "something"
+//     console.log('test2', test); // test2 gorsenecek stringdir something de stringdir gorsenecek 
+// }
+// console.log('test3', test); // test3 stringdir test variable ise undefined  
+
+
+// TASK5
+
+// let name = "Ulfat" = string
+// let surname = Zakirli error qaytaracaq
+// let year = 2000 number
+// year = "2000" string
+// let city undefined
+// let qualification = null object
+// let obj = { name: 'ulfat' }object
+// let arr = ['a', 'b', 'c'] object
+// console.log(typeof obj, typeof arr)
+
+
+// TASK 6
+
+// 1)
+
+// const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
+// let sum = 0
+// for (let i = 0; i < arr1.length; i++) {
+//     sum += arr1[i]
+// }
+// console.log(sum);
+
+// const addnum = arr1.push(10, 88)
+// console.log(arr1);
+// const removenumbers = arr1.shift()
+// arr1.shift()
+// console.log(arr1);
+
+// TASK 8
+
+// const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+// let even = []
+// numbers.forEach(element => {
+//     if (element % 1 == 0)
+//         even.push(element)
+// })
+// console.log(`Even numbers in an array are: ${even}`)
+
+// const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+// let odds = numbers.filter((numb) => numb % 2 === 1)
+// console.log(odds);
+
+// TASK 10
+// const myself = {
+//     FirstName: 'Fuad',
+//     Surname: 'Rasulov',
+//     Age: 24,
+//     Gender: 'Male',
+//     City: {
+//         Region: 'Baku',
+//         Street: 'Binagadi'
+//     }
+// }
+// myself.FirstName = 'Jony'
+// console.log(Gender)
+// console.log(City);
