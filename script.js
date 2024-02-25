2b5a61e0867f7d27b7ecb14c25766bbe8f9f5d9c
avrin002 <160397698+avrin002@users.noreply.github.com>
2024-02-25 13:27:39 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
-// task1//
+//  task1
 
 
-// 1) // let num = 12
-// // let num2 = 8
-// // console.log(num * num2)
+// function calc() {
 
-// 2)// let num = 10
-// // let num2 = 2
-// // let x = num / num2
-// // console.log(x);
+//     let num = 12
+//     let num2 = 8
+//     return console.log(num * num2)
+// }
+// calc()
 
-// 3) let num = 20
-// let num2 = 17
-// console.log(num + num2)
+
+// task 2
+// function calc() {
+
+//     let num = 10
+//     let num2 = 2
+//     let x = num / num2
+//     return console.log(x)
+// }
+// calc()
+
+
+// function calc() {
+
+//     let num = 20
+//     let num2 = 17
+//     return console.log(num + num2)
+
+// }
+// calc()
+
 
 // 4)
-// var name
-// var surname
-// var year
+// function info() {
+//     var name
+//     var surname
+//     var year
 
-// name = 'Fuad'
-// surname = 'Rasulov'
-// year = 1999
-// var info = name + ' ' + surname + ' ' + year
-// console.log(info);
-
-
+//     name = 'Fuad'
+//     surname = 'Rasulov'
+//     year = 1999
+//     var info = name + ' ' + surname + ' ' + year
+//     return console.log(info);
+// }
+// info()
 
 // let num = 17
 // let num2 = 12
 // let result = num / num12
 
 
-// // TASK 2
-// // // let a = 1, b = 1;
-// // // let c = ++a;
-// // // let d = b++;
-// // // console.log(c) = c de 2 cixacaq
-// // // console.log(d) = d de 2 cixacaq
-// console.log(b) = b de 1 ciaxacaq ;
+// function calc() {
+//     let a = 1,
+//         b = 1;
+//     let c = ++a;
+//     let d = b++;
+//     return console.log(c)
+//     return console.log(d)
+//     return console.log(b);
+// }
+// calc()
 
-
-// TASK 3
-// // let a = 2;
-// // let x = 1 + (a *= 2)
-// // console.log(x) = x de 5 cavabi cixacaq
-
+// function calc() {
+//     let a = 2;
+//     let x = 1 + (a *= 2)
+//     return console.log(x)
+// }
+// calc()
 
 
 // TASK 4
@@ -65,61 +86,79 @@
 // console.log('test3', test); // test3 stringdir test variable ise undefined  
 
 
-// TASK5
 
-// let name = "Ulfat" = string
-// let surname = Zakirli error qaytaracaq
-// let year = 2000 number
-// year = "2000" string
-// let city undefined
-// let qualification = null object
-// let obj = { name: 'ulfat' }object
-// let arr = ['a', 'b', 'c'] object
-// console.log(typeof obj, typeof arr)
 
+// function info() {
+//     let name = "Ulfat"
+//     let surname = 'Zakirli'
+//     let year = 2000
+//     year = "2000"
+//     let city
+//     let qualification = null
+//     let obj = { name: 'ulfat' }
+//     let arr = ['a', 'b', 'c']
+//     return console.log(typeof obj, typeof arr)
+// }
+// info()
 
 // TASK 6
 
 // 1)
 
-// const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
-// let sum = 0
-// for (let i = 0; i < arr1.length; i++) {
-//     sum += arr1[i]
+// function arr() {
+//     const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
+//     let sum = 0
+//     for (let i = 0; i < arr1.length; i++) {
+//         sum += arr1[i]
+//     }
+//     return console.log(sum);
 // }
-// console.log(sum);
+// arr()
 
-// const addnum = arr1.push(10, 88)
-// console.log(arr1);
-// const removenumbers = arr1.shift()
-// arr1.shift()
-// console.log(arr1);
+// function example() {
+//     const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
+//     const addnum = arr1.push(10, 88)
+//     console.log(arr1);
+//     const removenumbers = arr1.shift()
+//     arr1.shift()
+//     return console.log(arr1);
+// }
+// example()
 
 // TASK 8
+// function numbercalc() {
+//     const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+//     let even = []
+//     numbers.forEach(element => {
+//         if (element % 1 == 0)
+//             even.push(element)
+//     })
+//     return console.log(`Even numbers in an array are: ${even}`)
+// }
+// numbercalc()
 
-// const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
-// let even = []
-// numbers.forEach(element => {
-//     if (element % 1 == 0)
-//         even.push(element)
-// })
-// console.log(`Even numbers in an array are: ${even}`)
+// function calodd() {
+//     const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+//     let odds = numbers.filter((numb) => numb % 2 === 1)
+//     return console.log(odds);
+// }
+// calodd()
 
-// const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
-// let odds = numbers.filter((numb) => numb % 2 === 1)
-// console.log(odds);
 
 // TASK 10
-// const myself = {
-//     FirstName: 'Fuad',
-//     Surname: 'Rasulov',
-//     Age: 24,
-//     Gender: 'Male',
-//     City: {
-//         Region: 'Baku',
-//         Street: 'Binagadi'
+// function myself() {
+//     const myself = {
+//         FirstName: 'Fuad',
+//         Surname: 'Rasulov',
+//         Age: 24,
+//         Gender: 'Male',
+//         City: {
+//             Region: 'Baku',
+//             Street: 'Binagadi'
+//         }
 //     }
+//     myself.FirstName = 'Jony'
+//     return console.log(myself.Gender, myself.City);
+
 // }
-// myself.FirstName = 'Jony'
-// console.log(Gender)
-// console.log(City);
+// myself()
